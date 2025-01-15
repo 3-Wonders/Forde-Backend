aee4740342c819c6a2c7c739c113d232228f53ce
JeongseokHan <115478278+JeongseokHan@users.noreply.github.com>
2025-01-16 01:36:29 +0900

    당일 회의 테스트를 위한 commit
    테스트를 위해 merge 전, 해당 업무를 맡은 팀원의 코드를 복사 후 commit 하였습니다.

--- src/main/java/com/project/forde/entity/InterestTag.java
+++ src/main/java/com/project/forde/entity/InterestTag.java
@@ -1,5 +1,6 @@
 package com.project.forde.entity;
 
+import com.project.forde.entity.composite.InterestTagPK;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -12,22 +13,10 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "interest_tag")
 public class InterestTag {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "interest_tag_id", nullable = false, unique = true, columnDefinition = "INT UNSIGNED AUTO_INCREMENT")
-    private Long interestTagId;
-
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "tag_id", nullable = false, columnDefinition = "INT UNSIGNED")
-    private Tag tag;
-
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id",nullable = false, columnDefinition = "INT UNSIGNED")
-    private AppUser appUser;
+    @EmbeddedId
+    private InterestTagPK id;
 
     @CreationTimestamp
     @Column(name = "created_time", nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
-    private LocalDateTime createdTime; //생성된 시간
+    private LocalDateTime createdTime;
 }
